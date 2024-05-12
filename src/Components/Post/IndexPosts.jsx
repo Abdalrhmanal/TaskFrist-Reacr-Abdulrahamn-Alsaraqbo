7401734228ceb0431a40cdcbaf0c533fe21c5d5d
Abdulrahmanl <ramasy111020033@gmail.com>
2024-05-12 13:37:59 +0300

    Error Loding

--- src/Components/Post/IndexPosts.jsx
+++ src/Components/Post/IndexPosts.jsx
@@ -5,13 +5,24 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   const [posts, setPosts] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(data => setPosts(data))
-      .catch(error => console.error('Error fetching posts:', error));
+      .then(data => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+        setLoading(false);
+      });
   }, []);
 
+  if (loading) {
+    return <div>Loading posts...</div>;
+  }
   return (
     <>
 
